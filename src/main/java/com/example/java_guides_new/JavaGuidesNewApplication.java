4ebe39c48599108f2d85576286c11e648c82b2d2
Kapilan1998 <sriranjankapilan@gmail.com>
2023-03-30 17:49:51 +0530

    Initial commit

---
+++ src/main/java/com/example/java_guides_new/JavaGuidesNewApplication.java
@@ -0,0 +1,15 @@
+package com.example.java_guides_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGuidesNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGuidesNewApplication.class, args);
+
+        System.out.println("hi");
+    }
+
+}
